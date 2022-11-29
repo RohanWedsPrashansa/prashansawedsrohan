515d5b445fd58fce9b6e2392671075f37d8bcf3d
unknown <vivekkhodade01@gmail.com>
2022-11-30 01:42:48 +0530

    Updates

---
+++ precache-manifest.fd8ed5fe53f2a3a4553e45fdbb835ea1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de8abfc8364cae30f41",
+    "url": "/prashansawedsrohan/static/css/main.658d3cd6.chunk.css"
+  },
+  {
+    "revision": "8de8abfc8364cae30f41",
+    "url": "/prashansawedsrohan/static/js/main.8de8abfc.chunk.js"
+  },
+  {
+    "revision": "b6033a94dc22fe629ecd",
+    "url": "/prashansawedsrohan/static/js/runtime~main.b6033a94.js"
+  },
+  {
+    "revision": "adcec1ab2246c0138951",
+    "url": "/prashansawedsrohan/static/css/2.8a63fd22.chunk.css"
+  },
+  {
+    "revision": "adcec1ab2246c0138951",
+    "url": "/prashansawedsrohan/static/js/2.adcec1ab.chunk.js"
+  },
+  {
+    "revision": "ec959cf2e3c84958206aaf72608ebdb4",
+    "url": "/prashansawedsrohan/static/media/background2.ec959cf2.jpg"
+  },
+  {
+    "revision": "dda1d79f3aef9457f875ae113934d455",
+    "url": "/prashansawedsrohan/static/media/dp.dda1d79f.jpeg"
+  },
+  {
+    "revision": "9e050e136093bf73676534ea3f3a8b15",
+    "url": "/prashansawedsrohan/static/media/ganapati.9e050e13.jpeg"
+  },
+  {
+    "revision": "cdee0b035e5f33636e9dc12ac0c427e6",
+    "url": "/prashansawedsrohan/static/media/logo.cdee0b03.jpg"
+  },
+  {
+    "revision": "2725ee23af3c8073ab3c3c6e21e39198",
+    "url": "/prashansawedsrohan/static/media/second.2725ee23.jpg"
+  },
+  {
+    "revision": "090db8682aac3fdf4aabd804cea22ef8",
+    "url": "/prashansawedsrohan/static/media/hotel.090db868.jpg"
+  },
+  {
+    "revision": "18af806bc1de3249ab370f8b81eb9d02",
+    "url": "/prashansawedsrohan/static/media/SANGEET.18af806b.gif"
+  },
+  {
+    "revision": "51d5069a4a95e8e4a9a48befda72392a",
+    "url": "/prashansawedsrohan/static/media/carnival.51d5069a.gif"
+  },
+  {
+    "revision": "b67d985f84e28c9d0b31683072e96e81",
+    "url": "/prashansawedsrohan/static/media/Nikasi.b67d985f.gif"
+  },
+  {
+    "revision": "03c2984ed7b36ed9ce08177b312017b3",
+    "url": "/prashansawedsrohan/static/media/Phera.03c2984e.gif"
+  },
+  {
+    "revision": "d9eee02b07d291eb097c413c1d28b04d",
+    "url": "/prashansawedsrohan/static/media/awesome.d9eee02b.gif"
+  },
+  {
+    "revision": "fc32523a85113ebe4f1be6485bc418fd",
+    "url": "/prashansawedsrohan/static/media/last.fc32523a.jpeg"
+  },
+  {
+    "revision": "7409d7691ddca34f75b155583093d90e",
+    "url": "/prashansawedsrohan/static/media/selfies.7409d769.gif"
+  },
+  {
+    "revision": "28e6635ead6c8d09dd98fb1e56e708d9",
+    "url": "/prashansawedsrohan/static/media/tick.28e6635e.mp3"
+  },
+  {
+    "revision": "92aea05e365be0390a5132ac203ad861",
+    "url": "/prashansawedsrohan/static/media/kesariya.92aea05e.mp3"
+  },
+  {
+    "revision": "8ba9fcc10da0beaeeddd3c28dbd33223",
+    "url": "/prashansawedsrohan/index.html"
+  }
+];
